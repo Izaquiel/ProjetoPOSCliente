786e7e3bb9d0f49d243d6e47dc176f9f1f7917c9
Izaquiel <Izaquiel@IzaquielCruz>
2015-02-20 11:45:52 -0200

    Editar Usuario Finalizado.

--- src/main/java/managed/UsuarioManaged.java
+++ src/main/java/managed/UsuarioManaged.java
@@ -4,7 +4,11 @@ import com.sun.org.apache.xerces.internal.jaxp.datatype.XMLGregorianCalendarImpl
 import java.util.Date;
 import java.util.GregorianCalendar;
 import java.util.List;
+import java.util.Map;
 import javax.faces.bean.ManagedBean;
+import javax.faces.context.FacesContext;
+import javax.persistence.NoResultException;
+import javax.xml.datatype.XMLGregorianCalendar;
 import service.AgenciaInternaServiceService;
 import service.AgenciaInterno;
 import service.AgenciaService;
@@ -20,25 +24,33 @@ public class UsuarioManaged {
 
     Passagens passagens = new Passagens();
     AgenciaService port = passagens.getAgenciaServicePort();
-    
+
     AgenciaInternaServiceService agenciaInterna = new AgenciaInternaServiceService();
     AgenciaInterno portInterno = agenciaInterna.getAgenciaInternoPort();
-    
-    
+
     Usuario usuario = new Usuario();
-    Date dataAux;
-    
-    public void salvar(){
-        
-        GregorianCalendar cal = new GregorianCalendar(); 
-        cal.setTimeInMillis(dataAux.getTime());        
+    Usuario usuarioAux = new Usuario();
+    Date dataAux = new Date();
+
+    public void salvar() {
+
+        GregorianCalendar cal = new GregorianCalendar();
+        cal.setTimeInMillis(dataAux.getTime());
         XMLGregorianCalendarImpl data = new XMLGregorianCalendarImpl(cal);
-        
+
         usuario.setDataNascimento(data);
-        
+
         port.salvarUsuario(usuario);
         usuario = new Usuario();
         dataAux = null;
+    }
+
+    public Usuario getUsuarioAux() {
+        return usuarioAux;
+    }
+
+    public void setUsuarioAux(Usuario usuarioAux) {
+        this.usuarioAux = usuarioAux;
     }
 
     public Date getDataAux() {
@@ -48,16 +60,28 @@ public class UsuarioManaged {
     public void setDataAux(Date dataAux) {
         this.dataAux = dataAux;
     }
-    
-    public void editar(){
-        portInterno.editarUsuario(usuario);
+
+    public String editar() {
+        Map<String, String> params = FacesContext.getCurrentInstance().getExternalContext().getRequestParameterMap();
+        usuarioAux.setId(Long.parseLong(params.get("idUsuario")));
+
+        GregorianCalendar cal = new GregorianCalendar();
+        cal.setTimeInMillis(dataAux.getTime());
+        XMLGregorianCalendarImpl data = new XMLGregorianCalendarImpl(cal);
+
+        usuarioAux.setDataNascimento(data);
+        portInterno.editarUsuario(usuarioAux);
+
+        usuarioAux = new Usuario();
+        dataAux = null;
+        return null;
     }
-    
-    public List<Usuario> listarUsuarios(){
+
+    public List<Usuario> listarUsuarios() {
         return portInterno.getTodosUsuarios();
     }
-    
-    public Usuario getUsuarioPorLogin(String login){
+
+    public Usuario getUsuarioPorLogin(String login) {
         return port.getUsuario(login);
     }
 
@@ -76,5 +100,22 @@ public class UsuarioManaged {
     public void setUsuario(Usuario usuario) {
         this.usuario = usuario;
     }
-    
+
+    public String pesquisaUsuario() {
+        try {
+            usuarioAux = port.getUsuario(usuario.getCpf());
+
+            GregorianCalendar cal;
+            XMLGregorianCalendar data = usuarioAux.getDataNascimento();
+
+            cal = data.toGregorianCalendar();
+
+            dataAux.setTime(cal.getTimeInMillis());
+            
+            return "editarUsuario.xhtml";
+        } catch (NoResultException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
 }
